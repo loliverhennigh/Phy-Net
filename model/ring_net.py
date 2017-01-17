3afaf072b31764a01009b2ae3d5ee5f71f084e11
oliver <loliverhennigh101@gmail.com>
2017-01-17 12:08:31 -0600

    little more

--- model/ring_net.py
+++ model/ring_net.py
@@ -93,7 +93,7 @@ tf.app.flags.DEFINE_integer('init_unroll_length', 5,
                            """ unroll length to initialize network """)
 tf.app.flags.DEFINE_integer('unroll_length', 5,
                            """ unroll length """)
-tf.app.flags.DEFINE_bool('unroll_true', True,
+tf.app.flags.DEFINE_bool('unroll_from_true', True,
                            """ use the true data when unrolling the network (probably just used for unroll_length 1 when doing curriculum learning""")
 tf.app.flags.DEFINE_integer('restore_unroll_length', 0,
                            """ what to unroll length to restore from. (if 0 then initialize from scratch) """)
@@ -308,20 +308,117 @@ def discriminator(output, hidden_state=None):
 
   return label
 
-def encode_compress_decode(state, hidden_state=None):
-  
+def encode_compress_decode(state, boundry, hidden_state=None, z=None):
+ 
+  state = add_boundry(state, boundry)
+ 
   y_1 = encoding(state)
   if FLAGS.lstm:
     y_2, hidden_state = lstm_compression(y_1, hidden_state)
   else:
     y_2 = compression(y_1)
   if FLAGS.gan:
-    y_2 = add_z(y_2)
+    y_2 = add_z(y_2, z)
   x_2 = decoding(y_2) 
+
+  state = apply_boundry(state, boundry)
 
   return x_2, hidden_state
 
-def unroll(state
+def train_unroll():
+
+  total_unroll_length = FLAGS.init_unroll_length + FLAGS.unroll_length 
+
+  if FLAGS.gan:
+    z = tf.placeholder("float", [None, total_unroll_length, FLAGS.z_size])
+
+  state, boundry = inputs(FLAGS.batch_size, FLAGS.init_unroll_length + FLAGS.unroll_length)
+  
+  x_2_o = []
+  if FLAGS.gan:
+    x_2, hidden_state = encode_compress_decode(state[:,0], z=z[:,0], hidden_state=None)
+    gan_t_label, gan_t_hidden_state = discriminator(state[:,0], None)
+  else: 
+    x_2, hidden_state = encode_compress_decode(state[:,0], hidden_state=None)
+
+  tf.get_variable_scope().reuse_variable()
+
+  if FLAGS.gan:
+    gan_g_label, gan_g_hidden_state = ring_net.discriminator(state[:,0], None)
+
+  for i in xrange(FLAGS.init_unroll_length-1):
+    if FLAGS.gan: 
+      # unroll generator network 
+      x_2, hidden_state = ring_net.encode_compress_decode(state[:,i+1], hidden_state, z[:,i+1,:])
+  
+      # unroll discriminator network on true
+      gan_t_label, gan_t_hidden_state = ring_net.discriminator(state[:,i+1], gan_t_hidden_state)
+
+      # unroll discriminator network on generated
+      gan_g_label, gan_g_hidden_state = ring_net.discriminator(state[:,i+1], gan_g_hidden_state)
+
+    else:
+      # unroll generator network 
+      x_2, hidden_state = ring_net.encode_compress_decode(flow_boundry[:,i+1], hidden_state)
+
+  x_2_o.append(x_2)
+
+  if FLAGS.gan:
+    # unroll discriminator network on true
+    gan_t_label, gan_t_hidden_state = ring_net.discriminator(state[:,FLAGS.init_unroll_length], gan_t_hidden_state)
+
+    # unroll discriminator network on generated
+    gan_g_label, gan_g_hidden_state = ring_net.discriminator(x_2, gan_g_hidden_state)
+
+  for i in xrange(FLAGS.unroll_length):
+    if FLAGS.unroll_from_true:
+      next_state = state[:,i+FLAGS.init_unroll_length]
+    else:
+      next_state = x2
+
+    if FLAGS.gan: 
+      # unroll generator network 
+      x_2, hidden_state = ring_net.encode_compress_decode(next_state, hidden_state, z[:,i+FLAGS.init_unroll_length,:])
+  
+      # unroll discriminator network on true
+      gan_t_label, gan_t_hidden_state = ring_net.discriminator(state[:,i+FLAGS.init_unroll_length+1], gan_t_hidden_state)
+
+      # unroll discriminator network on generated
+      gan_g_label, gan_g_hidden_state = ring_net.discriminator(x_2, gan_g_hidden_state)
+
+    else:
+      # unroll generator network 
+      x_2, hidden_state = ring_net.encode_compress_decode(next_state, hidden_state)
+
+    x_2_o.append(x_2)
+
+  x_2_o = tf.pack(x_2_o)
+  x_2_o = tf.transpose(x_2_o, perm[1,0])
+
+
+
+def loss_mse(true, generated):
+  loss = tf.nn.l2_loss(true - generated)
+  tf.scalar_summary('reconstruction_loss', loss)
+  return loss
+   
+def loss_gan_true(true_label, generated_label):
+  loss_d_true = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(true_label, tf.ones_like(true_label)))
+  loss_d_generated = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(generated_label, tf.ones_like(generated_label)))
+  loss_d = loss_d_true + loss_d_generated
+  tf.scalar_summary('error discriminator true', loss_d_true)
+  tf.scalar_summary('error discriminator generated', loss_d_generated)
+  tf.scalar_summary('error discriminator', loss_d)
+  return loss_d
+ 
+def loss_gan_generated(generated_label):
+  error_g = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(generated_label, tf.ones_like(generated_label))) 
+  tf.scalar_summary('error generated', error_g)
+  return loss_g
+
+def loss(
+
+
 
 def train(total_loss, lr):
    #train_op = tf.train.AdamOptimizer(lr, epsilon=1.0).minimize(total_loss)
